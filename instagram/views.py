bdcbb7b87cedb9b2baebad78631d698428e96ed3
paulmburu08 <paulmburu08@gmail.com>
2020-07-04 02:29:14 +0300

    add templates

--- instagram/views.py
+++ instagram/views.py
@@ -3,6 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    HttpResponse = 'Yeessirr'
-    
-    render(request,HttpResponse)
+
+    render(request,'index.html')
