6e20b2e62f46d9277e10ef37638a8a21329d623c
paulmburu08 <paulmburu08@gmail.com>
2020-07-04 22:05:27 +0300

    add new_image function to views

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Profile,Image
-from .forms import ProfileForm
+from .forms import ProfileForm,PostImage
 
 # Create your views here.
 def index(request):
@@ -20,3 +20,17 @@ def profile(request):
         form = ProfileForm()
 
     return render(request, 'profile.html',{'form':form})
+
+def new_image(request):
+    if request.method == 'POST':
+        form = PostImage(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+        return redirect('new/post/')
+
+    else:
+        form = PostImage()
+
+    return render(request, 'new_post.html',{'form':form})
