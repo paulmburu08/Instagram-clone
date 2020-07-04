166225bb4ed23a0c72bd330e3df22d961acde5aa
paulmburu08 <paulmburu08@gmail.com>
2020-07-04 23:13:41 +0300

    add block styles

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url('^$',views.index,name='index'),
-    url(r'^profile$', views.profile, name='new-profile'),
+    url(r'^profile$', views.profile, name='profile'),
     url(r'^new/post$', views.new_post, name='new-post')
 ]
