3db994c91f9694c10d5c44843fd62ca76be47e0a
paulmburu08 <paulmburu08@gmail.com>
2020-07-04 16:58:18 +0300

    add Profile model

--- instagram/models.py
+++ instagram/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = CloudinaryField('image')
+    bio = models.TextField()
+
 class Image(models.Model):
     image = CloudinaryField('image')
     name = models.CharField(max_length=60)
