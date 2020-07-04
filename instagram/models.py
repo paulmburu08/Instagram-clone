223bdf9f652a1158fe72ec6f43817c78a5b8de1c
paulmburu08 <paulmburu08@gmail.com>
2020-07-04 17:16:26 +0300

    addlikes and comments to model

--- instagram/models.py
+++ instagram/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.db.models.signals import pre_delete
+import cloudinary
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
     profile_photo = CloudinaryField('image')
     bio = models.TextField()
 
+    @receiver(pre_delete, sender=Profile)
+    def photo_delete(self, sender, instance, **kwargs):
+        cloudinary.uploader.destroy(instance.image.public_id)
+
 class Image(models.Model):
     image = CloudinaryField('image')
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=30)
     caption = models.TextField()
+    profile = models.ForeignKey(Profile,on_delete=models.DO_NOTHING)
+    likes = models.IntegerField(default=0)
+    comments = models.CharField(max_length=100)
+
+    @receiver(pre_delete, sender=Image)
+    def photo_delete(self, sender, instance, **kwargs):
+        cloudinary.uploader.destroy(instance.image.public_id)
