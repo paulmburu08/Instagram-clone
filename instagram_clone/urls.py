21f48f72ffb866eb4b22176708c6eddec15ee283
paulmburu08 <paulmburu08@gmail.com>
2020-07-04 16:18:44 +0300

    chnage to django_registration folder

--- instagram_clone/urls.py
+++ instagram_clone/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls import url,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'',include('instagram.urls')),
-    url(r'^accounts/', include('registration.backends.hmac.urls')),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
