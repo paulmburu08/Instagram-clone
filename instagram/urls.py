57334d6ce2cf9d21d90ebc313b8381276d8fc6c3
paulmburu08 <paulmburu08@gmail.com>
2020-07-04 21:45:34 +0300

    add profile.html

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$',views.index,name='index')
+    url('^$',views.index,name='index'),
+    url(r'^new/profile$', views.profile, name='new-profile')
 ]
