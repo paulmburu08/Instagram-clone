37036a49ce894b36e4cdb87f3ca58f4f339f1df1
paulmburu08 <paulmburu08@gmail.com>
2020-07-04 18:50:25 +0300

    add profile to views

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Profile,Image
+from .forms import ProfileForm
 
 # Create your views here.
 def index(request):
 
     return render(request,'index.html')
+
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('')
+
+    else:
+        form = ProfileForm()
+
+    return render(request, 'profile.html',{'form':form})
